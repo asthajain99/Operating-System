cf65c032fc0b8a62d8e15fada5997e5057a2aa92
asthajain99 <46923265+asthajain99@users.noreply.github.com>
2019-04-02 11:56:57 +0530

    Add files via upload

---
+++ ThreadRunnable.java
@@ -0,0 +1,38 @@
+CODE
+class Demo implements Runnable
+{ 
+    public void run() 
+    { 
+        try
+        { 
+          System.out.println ("Thread " +Thread.currentThread().getId()+" is running"); 
+  	} 
+        catch (Exception ex) 
+        { 
+            System.out.println ("Exception is caught"); 
+        } 
+    } 
+} 
+   
+public class Multithread 
+{ 
+    public static void main(String[] args) 
+    { 
+        for (int i=0; i<8; i++) 
+        { 
+            Demo ob = new Demo();
+	    Thread th=new Thread(ob);	 
+            th.start(); 
+        } 
+    } 
+} 
+
+OUTPUT
+Thread 21 is running
+Thread 22 is running
+Thread 23 is running
+Thread 24 is running
+Thread 25 is running
+Thread 26 is running
+Thread 27 is running
+Thread 28 is running
